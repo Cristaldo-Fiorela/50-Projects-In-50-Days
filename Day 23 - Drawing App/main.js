7bc53deea412e5ccd37f873872e32cf69338143f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-05-15 18:11:47 -0300

    adding the mouse event listeners to be able to draw

--- Day 23 - Drawing App/main.js
+++ Day 23 - Drawing App/main.js
@@ -6,11 +6,46 @@ const canvas = document.querySelector('#canvas');
 // Variales
 const ctx = canvas.getContext('2d');
 let size =  20;
+let mouseIsPressed = false;
 let color =  'black';
 let x;
 let y;
 
+// Event Listeners
+canvas.addEventListener('mousedown', mousePress);
+canvas.addEventListener('mouseup', mousePress);
+canvas.addEventListener('mousemove', mouseMove);
+
 // Functions
+function mousePress(e) {
+    let eventType = e.type;
+
+    if( eventType === 'mousedown')  {
+        mouseIsPressed = true;
+
+        // provides the offset in the X/Y coordinate of the mouse pointer
+        x = e.offsetX;
+        y = e.offsetY;
+    } else {
+        mouseIsPressed = false;
+        x = undefined;
+        y = undefined;
+    }
+}
+
+function mouseMove(e) {
+    if(mouseIsPressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+
+        drawCircle(x2,y2);
+        drawLine(x, y, x2,y2);
+
+        x = x2;
+        y = y2;
+    }
+}
+
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2); // circle
@@ -25,9 +60,6 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
-    ctx.lineWidth = size;
+    ctx.lineWidth = size * 2;
     ctx.stroke();
 }
-
-drawCircle(100, 200)
-drawLine(300, 300, 300, 500)
