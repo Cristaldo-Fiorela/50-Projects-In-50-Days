97a31d50a70f3d70355b3208cf64cd9c25cb9137
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-05 21:47:03 -0300

    making notes with DOM

---
+++ Day 33 - Note App/main.js
@@ -0,0 +1,26 @@
+// * DOM SELECTORS
+const addBtn = document.querySelector('#add');
+
+// * EVENT LISTENERS
+addBtn.addEventListener('click', () => addNewNote());
+
+// * FUNCTIONS
+
+function addNewNote( text = '') {
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
+    <div class="tools">
+        <button class="edit">
+            <i class="fas fa-edit"></i>
+        </button>
+        <button class="delete">
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    </div>
+
+    <div class="main"></div>
+    <textarea></textarea>
+    `
+}
