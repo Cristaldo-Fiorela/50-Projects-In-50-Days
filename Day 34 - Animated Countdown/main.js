697713b0293a54d62f1ca34010e8a08fec3d123b
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-10 20:13:14 -0300

    playing counter animation each time the button is pressed and resetting the DOM.

--- Day 34 - Animated Countdown/main.js
+++ Day 34 - Animated Countdown/main.js
@@ -6,8 +6,23 @@ const replay = document.querySelector('#replay');
 
 runAnimation();
 // EVENT LISTENERS
+replay.addEventListener('click', () => {
+    resetDOM();
+    runAnimation();
+});
 
 // FUNCTIONS
+function resetDOM() {
+    counter.classList.remove('hide');
+    finalMessage.classList.remove('show');
+
+    numbers.forEach((number) => {
+        number.classList.value = '';
+    });
+
+    numbers[0].classList.add('in');
+}
+
 function runAnimation() {
     numbers.forEach((number, index) => {
         const nextToLast =  number.length - 1;
