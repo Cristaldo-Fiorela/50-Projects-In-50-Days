f4d30b0d529adc82a19f2206931998705d02f77f
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-14 21:00:01 -0300

    by adding the event listener to display the mouse and set/remove a color from our array

--- Day 36 - Hoverboard/main.js
+++ Day 36 - Hoverboard/main.js
@@ -12,5 +12,19 @@ for(let i = 0; i < squares; i++) {
     const square = document.createElement('DIV');
     square.classList.add('square');
 
+    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseout', () => removeColor(square));
+
     container.appendChild(square);
+}
+
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d';
+    element.style.boxShadow = '0 0 2px #000';
 }
