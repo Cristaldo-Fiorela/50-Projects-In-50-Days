e1288ee5e5336f600442e21801eed3676fbd9c6b
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-17 20:36:56 -0300

    fetching the data from de API

---
+++ Day 37 - Pokedex/main.js
@@ -0,0 +1,39 @@
+// DOM SELECTORS & VARIABLES
+const poke_container = document.querySelector('#poke-container');
+
+const pokemon_count =  150;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+}
+
+
+
+// EVENT LISTENERS
+
+// FUNCTIONS
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i);
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data)
+}
