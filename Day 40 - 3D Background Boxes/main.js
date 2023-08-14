d16adff70ff8c86199b96c0e823ce234c8b38012
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-08-14 19:17:47 -0300

    correcting box transitions on button click and making the values dynamic with js

---
+++ Day 40 - 3D Background Boxes/main.js
@@ -0,0 +1,33 @@
+// DOM SELECTORS
+const boxesContainer = document.querySelector('#boxes');
+const btn = document.querySelector('#btn');
+
+// EVENT LISTENER
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'));
+
+// FUNCTIONS
+function createBoxes() {
+    for(let i = 0; i < 4; i++) {
+        for(let b = 0; b < 4; b++) {
+            const box = document.createElement('DIV');
+            box.classList.add('box');
+            box.style.backgroundPosition = `${-b * 125}px ${-i * 125}px`; // X VALUE - Y VALUE
+            boxesContainer.appendChild(box);
+        }
+    }
+}
+createBoxes();
+
+
+{/* <div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div>
+<div class="box"></div> */}
